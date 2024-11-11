f2c5ee2d001184c82754ce11c3fd748101a3986b
Leo D. Angelo <104923003+Sarutobikazuya3003@users.noreply.github.com>
2024-11-11 22:59:18 +0200

    Delete customer-portal-api-secure-V2 directory

--- customer-portal-api-secure-V2/backend/backend/routes/paymentRoutes.js
+++
@@ -1,8 +0,0 @@
-// backend/routes/paymentRoutes.js
-const express = require('express');
-const { makePayment } = require('../controllers/paymentController');
-const router = express.Router();
-
-router.post('/makePayment', makePayment);
-
-module.exports = router;
--- customer-portal-api-secure-V2/backend/backend/routes/paymentRoutesa.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const PaymentController = require('../controllers/paymentControllera');  // Adjust path if needed
-
-// Define route for fetching all payments
-router.get('/', PaymentController.getAllPayments);  // '/payment/allPayments' is handled here
-
-module.exports = router;
